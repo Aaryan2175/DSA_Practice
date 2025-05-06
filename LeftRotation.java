ca98a6e0d6e8ce51bca04d2d2b1291b75a26a29d
aary75 <101628957+aary75@users.noreply.github.com>
2025-05-06 11:56:15 +0530

    Add files via upload

---
+++ LeftRotation.java
@@ -0,0 +1,74 @@
+package DSAProblems;
+
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class LeftRotationSolution {
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        // Write your code here
+        if(arr.size() == 0) return arr;
+
+        int turn = d%arr.size();
+        List<Integer> newList = new ArrayList<>();
+
+        for(int i = turn;i<arr.size();i++){
+            newList.add(arr.get(i));
+        }
+
+        for(int i=0;i<turn;i++){
+            newList.add(arr.get(i));
+        }
+
+        return newList;
+
+    }
+
+}
+
+public class LeftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = LeftRotationSolution.rotateLeft(d, arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
