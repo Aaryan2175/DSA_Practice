3934e06dd80611a91e178b4ae7cfee717f528ad9
aary75 <101628957+aary75@users.noreply.github.com>
2025-05-12 19:32:17 +0530

    Update DesignTaskManager.java

--- DesignTaskManager.java
+++ DesignTaskManager.java
@@ -69,3 +69,74 @@ class TaskManager {
  * obj.rmv(taskId);
  * int param_4 = obj.execTop();
  */
+
+
+
+// second approach(for last 3 cases, TLE is coming)
+// class Task{  
+//      int userId, taskId, priority;
+
+//      Task(int userId, int taskId, int priority){
+//         this.userId = userId;
+//         this.taskId = taskId;
+//         this.priority = priority;
+//      }
+// }
+
+// class TaskManager {
+
+//     List<Task> ans;
+//     public TaskManager(List<List<Integer>> tasks) {
+//         ans = new ArrayList<>();
+//           for (List<Integer> t : tasks) {
+//             ans.add(new Task(t.get(0),t.get(1),t.get(2)));
+//         }
+//     }
+    
+//     public void add(int userId, int taskId, int priority) {
+//         ans.add(new Task(userId,taskId,priority));
+//     }
+    
+//     public void edit(int taskId, int newPriority) {
+//         for(Task task: ans){
+//             if(task.taskId == taskId){
+//                 task.priority = newPriority;
+//                 break;
+//             }
+//         }
+//     }
+    
+//     public void rmv(int taskId) {
+//         Iterator<Task> it = ans.iterator();
+
+//         while(it.hasNext()){
+//             if(it.next().taskId == taskId){
+//                 it.remove();
+//                 break;
+//             }
+//         }
+//     }
+    
+//     public int execTop() {
+//         if(ans.size() == 0) return -1;
+
+//         // Task topTask = null;
+//         for(Task task : ans){
+//             if(topTask == null || task.priority > topTask.priority || (task.priority  == topTask.priority && task.taskId > topTask.taskId)){
+//                 topTask = task;
+//             } 
+//         }
+
+//         ans.remove(topTask);
+//         return topTask.userId;
+//     }
+// }
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager obj = new TaskManager(tasks);
+ * obj.add(userId,taskId,priority);
+ * obj.edit(taskId,newPriority);
+ * obj.rmv(taskId);
+ * int param_4 = obj.execTop();
+ */
